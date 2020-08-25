7561a45e8b0d929cb25d679f3ca403f3d79d3bd7
gustavo867 <tartatavo3@gmail.com>
2020-08-25 10:22:24 -0300

    :sparkles: adding types and colors

--- src/pages/Pokemons/index.tsx
+++ src/pages/Pokemons/index.tsx
@@ -4,20 +4,14 @@ import {
   Text,
   StyleSheet,
   FlatList,
-  ScrollView,
   Image,
   Dimensions,
+  SafeAreaView,
 } from 'react-native';
 import axios from 'axios';
-import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
 
 const { height, width } = Dimensions.get('window');
-
-interface PokeData {
-  name: string;
-  id: number;
-  types: string;
-}
 
 interface TypeOject {
   name: string;
@@ -28,161 +22,99 @@ interface Type {
   slot: number;
   type: TypeOject;
 }
-
 interface Data {
   name: string;
-  id: number;
-  types: Type;
+  url: string;
+  color: string;
 }
 
 const Pokemons: React.FC = () => {
-  const [page, setPage] = useState(20);
+  const [page, setPage] = useState(10);
   const [offset, setOffset] = useState(0);
-  const [pokemons, setPokemons] = useState([]);
-  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/1/');
-  const [id, setId] = useState(0);
-  const [name, setName] = useState('');
-  const [types, setTypes] = useState<Type[]>();
-  const [data, setData] = useState<Data[]>([]);
+  const [data, setData] = useState<Data | any>([]);
 
   async function loadPokemons(limit = page) {
-    await axios
-      .get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
-      .then((response) => {
-        const data = response.data;
-
-        setPokemons(data);
-
-        const pokemonResults = data.results;
-
-        if (pokemonResults === null) {
-          return;
-        }
-
-        pokemonResults.forEach((element: any) => {
-          setUrl(element.url);
-        });
-
-        const pokemonNames = pokemonResults.map((item: any) => item.name);
-
-        setPage(page + 20);
-        setOffset(offset + 20);
-      });
-  }
-
-  async function loadUrl() {
-    await axios.get(url).then((response) => {
+    await api.get(`offset=${offset}&limit=${limit}`).then((response) => {
       const data = response.data;
 
-      setData(data);
-      setName(data.name);
-      setId(data.id);
-      setTypes(data.types);
+      setData(data.results);
+      setOffset(offset + 10);
+      setPage(page + 10);
     });
   }
 
   useEffect(() => {
-    async function load() {
-      await loadPokemons();
-      await loadUrl();
-    }
-    load();
+    loadPokemons();
   }, []);
 
-  {
-    pokemons.map === undefined && <Text style={styles.text}>Loading</Text>;
-  }
+  // useEffect(() => {
+  //   async function loadTypes() {
+  //     await axios.get(url).then((response) => {
+  //       const data = response.data;
+  //       const types = data.types;
 
-  // Card
-  const Item = (item: any, index: number) => {
+  //       types.forEach((element: Type) => {
+  //         // console.log(element.slot);
+  //       });
+
+  //       setTypes(types);
+  //     });
+  //   }
+  //   loadTypes();
+  // }, []);
+
+  const Item = (item: Data) => {
+    const urls = item.url;
+
+    const pokeId = urls
+      .replace('https://pokeapi.co/api/v2/pokemon/', '')
+      .replace('/', '');
+
+    const colorsUrl = `https://pokeapi.co/api/v2/pokemon-color/${pokeId}`;
+
+    // async function loadColor() {
+    //   await axios.get(colorsUrl).then((response) => {
+    //     const colorData = response.data;
+    //     const color = colorData.name;
+
+    //     setColors(color);
+
+    //     // console.log(colors);
+    //   });
+    // }
+
     return (
-      <View key={index}>
-        <Text style={styles.text}>{item.name}</Text>
+      <View key={item.name} style={[styles.card]}>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={{
+            uri: `https://pokeres.bastionbot.org/images/pokemon/${pokeId}.png`,
+          }}
+        />
+        <View style={styles.bottomCard}>
+          <Text style={styles.pokemonName}>{item.name}</Text>
+          {/* {types.map((item: Type, index: number) => {
+            return <Text key={index}>{item.type.name}</Text>;
+          })} */}
+        </View>
       </View>
     );
   };
 
-  // const dados = data?.forEach((element) => {
-  //   console.log(element.name);
-  // });
-
-  console.log(data);
-
-  if (data == null) {
-    return (
-      <View
-        style={{
-          backgroundColor: '#ccc',
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Text style={styles.loading}>Loading</Text>
-      </View>
-    );
-  }
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.pokemons}>Pokemons</Text>
-      <ScrollView
-        style={{ paddingTop: 20 }}
-        bounces={false}
-        decelerationRate="fast"
-        showsVerticalScrollIndicator={false}
-      >
-        {/* Card Provisorio */}
-        {/* {data.map((item) => {
-          return (
-            <View style={styles.card}>
-              <Image
-                resizeMode="contain"
-                style={styles.image}
-                source={{
-                  uri: `https://pokeres.bastionbot.org/images/pokemon/${item.id}.png`,
-                }}
-              />
-              <View style={styles.bottomCard}>
-                <Text style={styles.pokemonName}>{item.name}</Text>
-                <Text>#{item.id}</Text>
-                {types?.map((item, index) => {
-                  return <Text key={index}>{item.type.name}</Text>;
-                })}
-              </View>
-            </View>
-          );
-        })} */}
-
-        {/* // Nome dos pokemons */}
-        {/* {pokemonsNames.map((item: any, index: number) => {
-          return (
-            <View
-              style={{ alignItems: 'center', justifyContent: 'center' }}
-              key={index}
-            >
-              <Text style={styles.text}>{item}</Text>
-            </View>
-          );
-        })} */}
-
-        {/* {pokeData!.map((item: Item) => {
-          return (
-            <View>
-              <Text>{item.name}</Text>
-              <Text>{item.id}</Text>
-            </View>
-          );
-        })} */}
-      </ScrollView>
-      {/* <FlatList
-        keyExtractor={(item: any) => item.id}
+      <FlatList
         data={data}
+        keyExtractor={(item: any) => item.name}
         renderItem={({ item }: any) => <Item {...item} />}
-      /> */}
-      <RectButton onPress={() => loadPokemons()} style={styles.loadButton}>
-        <Text style={styles.load}>Load More Pokemons</Text>
-      </RectButton>
-    </View>
+        bounces={true}
+        showsVerticalScrollIndicator={false}
+        onEndReached={() => loadPokemons()}
+        onEndReachedThreshold={0.1}
+      />
+    </SafeAreaView>
   );
 };
 
@@ -191,7 +123,7 @@ export default Pokemons;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 20,
+    paddingTop: height * 0.05,
     backgroundColor: 'rgba(0, 0, 0, 0.6)',
     alignItems: 'center',
   },
@@ -217,6 +149,15 @@ const styles = StyleSheet.create({
     fontSize: 25,
     color: '#FFF',
   },
+  ability: {
+    color: '#010101',
+    fontSize: 14,
+    letterSpacing: 1,
+  },
+  loading: {
+    fontSize: 24,
+    letterSpacing: 1,
+  },
   image: {
     height: height / 2.5,
     width: width,
@@ -225,8 +166,8 @@ const styles = StyleSheet.create({
   card: {
     height: height / 1.6,
     width: width * 0.9,
-    backgroundColor: 'rgba(255, 255, 255, 0.6)',
     borderRadius: 8,
+    margin: 20,
   },
   bottomCard: {
     backgroundColor: '#FFF',
@@ -238,14 +179,5 @@ const styles = StyleSheet.create({
     color: '#010101',
     fontSize: 24,
     letterSpacing: 1.5,
-  },
-  ability: {
-    color: '#010101',
-    fontSize: 14,
-    letterSpacing: 1,
-  },
-  loading: {
-    fontSize: 24,
-    letterSpacing: 1,
   },
 });
