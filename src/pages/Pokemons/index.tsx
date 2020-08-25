107ad79f024f108c136d8b0df163b2e5ae5fa766
gustavo867 <tartatavo3@gmail.com>
2020-08-25 10:43:00 -0300

    :tada: finished first page

--- src/pages/Pokemons/index.tsx
+++ src/pages/Pokemons/index.tsx
@@ -7,6 +7,7 @@ import {
   Image,
   Dimensions,
   SafeAreaView,
+  ActivityIndicator,
 } from 'react-native';
 import axios from 'axios';
 import api from '../../services/api';
@@ -29,17 +30,20 @@ interface Data {
 }
 
 const Pokemons: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(10);
   const [offset, setOffset] = useState(0);
   const [data, setData] = useState<Data | any>([]);
 
   async function loadPokemons(limit = page) {
+    setLoading(true);
     await api.get(`offset=${offset}&limit=${limit}`).then((response) => {
       const data = response.data;
 
       setData(data.results);
       setOffset(offset + 10);
       setPage(page + 10);
+      setLoading(false);
     });
   }
 
@@ -47,41 +51,12 @@ const Pokemons: React.FC = () => {
     loadPokemons();
   }, []);
 
-  // useEffect(() => {
-  //   async function loadTypes() {
-  //     await axios.get(url).then((response) => {
-  //       const data = response.data;
-  //       const types = data.types;
-
-  //       types.forEach((element: Type) => {
-  //         // console.log(element.slot);
-  //       });
-
-  //       setTypes(types);
-  //     });
-  //   }
-  //   loadTypes();
-  // }, []);
-
   const Item = (item: Data) => {
     const urls = item.url;
 
     const pokeId = urls
       .replace('https://pokeapi.co/api/v2/pokemon/', '')
       .replace('/', '');
-
-    const colorsUrl = `https://pokeapi.co/api/v2/pokemon-color/${pokeId}`;
-
-    // async function loadColor() {
-    //   await axios.get(colorsUrl).then((response) => {
-    //     const colorData = response.data;
-    //     const color = colorData.name;
-
-    //     setColors(color);
-
-    //     // console.log(colors);
-    //   });
-    // }
 
     return (
       <View key={item.name} style={[styles.card]}>
@@ -94,9 +69,6 @@ const Pokemons: React.FC = () => {
         />
         <View style={styles.bottomCard}>
           <Text style={styles.pokemonName}>{item.name}</Text>
-          {/* {types.map((item: Type, index: number) => {
-            return <Text key={index}>{item.type.name}</Text>;
-          })} */}
         </View>
       </View>
     );
@@ -109,10 +81,18 @@ const Pokemons: React.FC = () => {
         data={data}
         keyExtractor={(item: any) => item.name}
         renderItem={({ item }: any) => <Item {...item} />}
-        bounces={true}
+        bounces={false}
         showsVerticalScrollIndicator={false}
         onEndReached={() => loadPokemons()}
         onEndReachedThreshold={0.1}
+        numColumns={2}
+        viewabilityConfig={{ viewAreaCoveragePercentThreshold: 10 }}
+        ListFooterComponent={
+          (loading && (
+            <ActivityIndicator color="#000" size="small" animating={loading} />
+          )) ||
+          null
+        }
       />
     </SafeAreaView>
   );
@@ -132,42 +112,28 @@ const styles = StyleSheet.create({
     fontSize: 20,
     letterSpacing: 2,
   },
-  load: {
-    color: '#010101',
-    fontSize: 22,
-  },
-  loadButton: {
-    marginTop: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '80%',
-    height: 56,
-    backgroundColor: '#FFFFFF',
-    marginBottom: 20,
-  },
   pokemons: {
     fontSize: 25,
     color: '#FFF',
-  },
-  ability: {
-    color: '#010101',
-    fontSize: 14,
-    letterSpacing: 1,
   },
   loading: {
     fontSize: 24,
     letterSpacing: 1,
   },
   image: {
-    height: height / 2.5,
-    width: width,
+    height: height / 3.4,
+    width: width / 2.8,
     alignSelf: 'center',
   },
   card: {
-    height: height / 1.6,
-    width: width * 0.9,
+    height: height / 2.0,
+    width: '45%',
     borderRadius: 8,
-    margin: 20,
+    margin: 10,
+    flexGrow: 1,
+    flexShrink: 1,
+    borderColor: 'rgba(255, 255, 255, 0.8)',
+    borderWidth: 1,
   },
   bottomCard: {
     backgroundColor: '#FFF',
@@ -177,7 +143,7 @@ const styles = StyleSheet.create({
   },
   pokemonName: {
     color: '#010101',
-    fontSize: 24,
+    fontSize: 15,
     letterSpacing: 1.5,
   },
 });
